f204199d20df657b33b55013fca45c386565e0ad
Roberto Eustaquio <robertoeb.jr@gmail.com>
2020-03-29 19:19:25 -0300

    Add be-the-hero frontend

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+
+import api from "../../services/api";
+import "./styles.css";
+
+import logoImg from "../../assets/logo.svg";
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+  const [isSubmiting, setIsSubmiting] = useState(false);
+  const [responseMessage, setResponseMessage] = useState("");
+  const [error, setError] = useState(false);
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    setIsSubmiting(true);
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    };
+
+    try {
+      const response = await api.post("/ongs", data);
+
+      setResponseMessage(
+        `ONG cadastrada com sucesso! Seu ID de acesso: ${response.data.id}`
+      );
+
+      setName("");
+      setEmail("");
+      setWhatsapp("");
+      setCity("");
+      setUf("");
+    } catch (err) {
+      setError(true);
+      setResponseMessage("Error no cadastro, tente novamente.");
+    }
+  }
+
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
+
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
+            os casos da sua ONG.
+          </p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041" /> Voltar para o logon
+          </Link>
+        </section>
+        <form onSubmit={handleRegister}>
+          <input
+            value={name}
+            onChange={e => setName(e.target.value)}
+            type="text"
+            placeholder="Nome da ONG"
+          />
+          <input
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            type="email"
+            placeholder="E-mail"
+          />
+          <input
+            value={whatsapp}
+            onChange={e => setWhatsapp(e.target.value)}
+            type="text"
+            placeholder="WhatsApp"
+          />
+
+          <div className="input-group">
+            <input
+              value={city}
+              onChange={e => setCity(e.target.value)}
+              type="text"
+              placeholder="Cidade"
+            />
+            <input
+              value={uf}
+              onChange={e => setUf(e.target.value)}
+              type="text"
+              placeholder="UF"
+              style={{ width: 80 }}
+            />
+          </div>
+
+          <button className="button" type="submit" disabled={isSubmiting}>
+            Cadastrar
+          </button>
+          <small className={error ? "error" : "success"}>
+            {responseMessage}
+          </small>
+        </form>
+      </div>
+    </div>
+  );
+}
